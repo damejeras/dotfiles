9ec4104c167db3131a345984c0c294b92e75b9b9
Darius Mejeras <darius@mejeras.lt>
2024-01-30 13:06:05 +0200

    Add dap

--- .config/nvim/lua/plugins/neovim.lua
+++ .config/nvim/lua/plugins/neovim.lua
@@ -17,9 +17,9 @@ return {
       -- document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]o', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]unk', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
