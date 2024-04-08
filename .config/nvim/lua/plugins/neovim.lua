3d1a1a38fe32a69e3d2f564b680dfdeebb2ff20c
Darius Mejeras <darius@mejeras.lt>
2024-04-08 14:45:36 +0300

    Better colors

--- .config/nvim/lua/plugins/neovim.lua
+++ .config/nvim/lua/plugins/neovim.lua
@@ -173,5 +173,13 @@ return {
   {
     'm4xshen/autoclose.nvim',
     opts = {},
+  },
+
+  -- Preview markdown
+  {    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+
   }
 }
